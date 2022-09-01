8bcae43d131860914cb05d784d7979ed643d5776
Alex <al.repin.a@yandex.ru>
2022-09-01 11:31:56 +0300

    add Task029

---
+++ Task029/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] ArrayRandom(int num)
+{
+    int[] arr = new int[num];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100);
+    }
+    return arr;
+}
+
+int[] array = ArrayRandom(num);
+foreach(var item in array)
+{
+    Console.Write(item + ", ");
+}
+//Console.WriteLine($"[{string.Join(", ", ArrayRandom())}]");
